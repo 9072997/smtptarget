1ca006f3f3692670f5dd2b85349c8c4c2e6caed7
Jon Penn <jpenn@bentonvillek12.org>
2020-07-08 14:24:13 -0500

    stutter less

--- smtptarget.go
+++ smtptarget.go
@@ -16,9 +16,9 @@ import (
 	"time"
 )
 
-// SMTPTarget represents a destination to which mail may be sent
-type SMTPTarget struct {
-	server   string
+// Target represents a destination to which mail may be sent
+type Target struct {
+	addr     string
 	username string
 	password string
 	timeout  time.Duration
@@ -31,15 +31,15 @@ type SMTPTarget struct {
 	closed           *bool
 }
 
-// New creates a new SMTPTarget to which you can send mail. server should be
-// a host and port in the format "smtp.example.com:587". Username and
-// password are required. Anonymous authentication is not supported. timeout
-// is how long the connection will be allowed to sit idle before it is
+// New creates a new Target to which you can send mail. host should be a
+// host and port in the format "smtp.example.com:587". Username and password
+// are required. Anonymous authentication is not supported. timeout is how
+// long the connection will be allowed to sit idle before it is
 // disconnected. It will automatically re-connect next time you call
 // SendMail().
-func New(server, username, password string, timeout time.Duration) SMTPTarget {
-	t := SMTPTarget{
-		server:          server,
+func New(addr, username, password string, timeout time.Duration) Target {
+	t := Target{
+		addr:            addr,
 		username:        username,
 		password:        password,
 		timeout:         timeout,
@@ -75,16 +75,16 @@ func New(server, username, password string, timeout time.Duration) SMTPTarget {
 }
 
 // Close the connection to the SMTP server and end the possibleTimeout
-// watcher thread. The SMTPTarget cannot be used after this. You must make
-// a new one.
-func (t SMTPTarget) Close() error {
+// watcher thread. The Target cannot be used after this. You must make a new
+// one.
+func (t Target) Close() error {
 	// can't be sending mail while we disconnect
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
 
 	// don't try to close a channel twice
 	if *t.closed {
-		return errors.New("SMTPTarget is already closed")
+		return errors.New("Target is already closed")
 	}
 
 	// so we know the possibleTimeout watcher is dead or dieing
@@ -102,9 +102,9 @@ func (t SMTPTarget) Close() error {
 }
 
 // SendMail works like smtp.SendMail() except that it uses the connection
-// from the SMTPTarget. It will first re-establish the connection if the
+// from the Target. It will first re-establish the connection if the
 // connection is unhealthy or not connected.
-func (t SMTPTarget) SendMail(from string, to []string, msg []byte) error {
+func (t Target) SendMail(from string, to []string, msg []byte) error {
 	// can't send more than one message at a time
 	t.mutex.Lock()
 	defer t.mutex.Unlock()
@@ -112,7 +112,7 @@ func (t SMTPTarget) SendMail(from string, to []string, msg []byte) error {
 	// if the target is closed there is no possibleTimeout watcher, so we
 	// don't want to open a new connection
 	if *t.closed {
-		return errors.New("SMTPTarget is closed")
+		return errors.New("Target is closed")
 	}
 
 	// ensure the client is connected and in a known state
@@ -160,7 +160,7 @@ func (t SMTPTarget) SendMail(from string, to []string, msg []byte) error {
 // ensure there is a functioning connection to the SMTP server and that it
 // is in a known state
 // NOTE: this is NOT threadsafe
-func (t SMTPTarget) reset() (err error) {
+func (t Target) reset() (err error) {
 	switch {
 	// we have a connection, but something is wrong with it
 	case t.client != nil && t.client.Noop() != nil:
@@ -170,33 +170,33 @@ func (t SMTPTarget) reset() (err error) {
 	// we don't have a connection
 	case t.client == nil:
 		// open the connection to the server
-		t.client, err = smtp.Dial(t.server)
+		t.client, err = smtp.Dial(t.addr)
 		if err != nil {
 			return err
 		}
 
 		// identify ourselves
-		localName, err := os.Hostname()
+		localHost, err := os.Hostname()
 		if err != nil {
-			localName = "localhost"
+			localHost = "localhost"
 		}
-		err = t.client.Hello(localName)
+		err = t.client.Hello(localHost)
 		if err != nil {
 			return err
 		}
 
 		// start TLS (encryption)
-		serverName, _, err := net.SplitHostPort(t.server)
+		serverHost, _, err := net.SplitHostPort(t.addr)
 		if err != nil {
 			return err
 		}
-		err = t.client.StartTLS(&tls.Config{ServerName: serverName})
+		err = t.client.StartTLS(&tls.Config{ServerName: serverHost})
 		if err != nil {
 			return err
 		}
 
 		// authenticate
-		auth := smtp.PlainAuth("", t.username, t.password, serverName)
+		auth := smtp.PlainAuth("", t.username, t.password, serverHost)
 		err = t.client.Auth(auth)
 		if err != nil {
 			return err
@@ -214,7 +214,7 @@ func (t SMTPTarget) reset() (err error) {
 
 // close the connection to the SMTP server
 // NOTE: this is NOT threadsafe
-func (t SMTPTarget) disconnect() error {
+func (t Target) disconnect() error {
 	if t.client == nil {
 		return nil
 	}
